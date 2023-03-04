e5a4821adb99d6656cee24a28ea8efe4556a9a8f
Nauany camille <s2nauanycamille@gmail.com>
2023-03-03 21:27:44 -0300

    atividade

---
+++ script.js
@@ -0,0 +1,13 @@
+var texto = "oi tudo bem";
+var num= '123456';
+var meunome="Nauany";
+var minhaidade =19;
+var textto = `Meu nome é ${nome} e minha idade é ${minhaidade} anos.`;
+/*"meu nome é "+nome+"........";*/
+var sobrenomemeu = "camille"
+var completo = `${meunome} ${sobrenomemeu}`;
+var ingles = "It's study time";
+
+console.log(completo);
+console.log(textto);
+console.log(typeof meunome);
